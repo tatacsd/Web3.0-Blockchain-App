8a3ed60cb4df0271fb760b355743d5f38277dc22
thayscasado <thayscasado@hotmail.com>
2022-02-20 22:27:43 -0800

    components structure

---
+++ client/src/components/index.js
@@ -0,0 +1,7 @@
+// export all the components in a singles file
+export {default as Footer} from './Footer';
+export {default as Loader} from './Loader';
+export {default as Navbar} from './Navbar';
+export {default as Services} from './Services';
+export {default as Transactions} from './Transactions';
+export {default as Welcome} from './Welcome';
